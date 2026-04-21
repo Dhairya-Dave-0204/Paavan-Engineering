1e73e8bc1a835f63bbcc9116fc9b2e6e78ed7c37
Dhairya <dhairyadave962@gmail.com>
2026-04-21 12:41:08 +0530

    Add the assets and configure the usage

---
+++ src/assets/assets.js
@@ -0,0 +1,27 @@
+import Coils1 from "./Coils1.jpeg"
+import Coils2 from "./Coils2.jpeg"
+import Machine1 from "./Machine1.jpeg"
+import Machine2 from "./Machine2.jpeg"
+import Machine3 from "./Machine3.jpeg"
+import Machine4 from "./Machine4.jpeg"
+import Machine5 from "./Machine5.jpeg"
+import Machine6 from "./Machine6.jpeg"
+import Machine7 from "./Machine7.jpeg"
+import Machine8 from "./Machine8.jpeg"
+import Machine9 from "./Machine9.jpeg"
+import PaavanLogo from "./PaavanLogo.png"
+
+export {
+    Coils1,
+    Coils2,
+    Machine1,
+    Machine2,
+    Machine3,
+    Machine4,
+    Machine5,
+    Machine6,
+    Machine7,
+    Machine8,
+    Machine9,
+    PaavanLogo,
+}
