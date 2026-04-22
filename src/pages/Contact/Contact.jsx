6dfb80c47cbf0a39c651a761103829d0489ab2ef
Dhairya <dhairyadave962@gmail.com>
2026-04-22 18:52:59 +0530

    Create the structure of the contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { ContactHero } from "../../components/component_index"
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <>
+      <ContactHero />
+    </>
   )
 }
 
