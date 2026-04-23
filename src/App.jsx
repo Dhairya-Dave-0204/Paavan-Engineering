98b979321231f77f4c6a4f39a3334461e9010adf
Dhairya <dhairyadave962@gmail.com>
2026-04-23 11:08:57 +0530

    Create the individual product detail page and its routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { Home, About, Contact, Products } from "./pages/page_index";
+import {
+  Home,
+  About,
+  Contact,
+  Products,
+  ProductDetail,
+} from "./pages/page_index";
 import { Navbar, Footer } from "./components/component_index";
 
 function App() {
@@ -12,6 +18,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/products/:slug" element={<ProductDetail />} />
       </Routes>
       <Footer />
     </>
--- src/pages/page_index.js
+++ src/pages/page_index.js
@@ -2,10 +2,12 @@ import Home from "./Home/Home";
 import About from "./About/About";
 import Contact from "./Contact/Contact";
 import Products from "./Products/Products";
+import ProductDetail from "./ProductDetail/ProductDetail";
 
 export {
     Home,
     About,
     Contact,
     Products,
+    ProductDetail,
 }
