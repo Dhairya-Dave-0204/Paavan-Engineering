c839430bc0ce6862bb28389f3e4f97e6b28d4578
Dhairya <dhairyadave962@gmail.com>
2026-04-26 12:01:16 +0530

    Enlarge the whatsapp icon

--- src/pages/WhatsAppButton/WhatsAppButton.jsx
+++ src/pages/WhatsAppButton/WhatsAppButton.jsx
@@ -11,7 +11,7 @@ export default function WhatsAppButton() {
       href={whatsappUrl}
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed z-50 flex items-center justify-center text-2xl text-white transition-all duration-300 bg-green-500 rounded-full shadow-md bottom-6 right-6 w-14 h-14 hover:scale-110 hover:shadow-lg"
+      className="fixed z-50 flex items-center justify-center text-3xl text-white transition-all duration-300 bg-green-500 rounded-full shadow-md bottom-6 right-6 w-14 h-14 hover:scale-110 hover:shadow-lg"
     >
       <i className="ri-whatsapp-line"></i>
     </a>
